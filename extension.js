0a3fcbccf6d6852fa7b5e47c23635c7561444ee7
RdapHQ <rdaphq@gmail.com>
2026-05-19 14:55:44 -0500

    fix(discord rpc): use session start for presence elapsed time
    add [Traecord] prefix to all console logs for better filtering
    remove outdated commented legacy code from extension.js

--- extension.js
+++ extension.js
@@ -2,8 +2,10 @@ const vscode = require('vscode');
 
 let rpcProcess;
 let isConnected = false;
+let sessionStartTimestamp;
 
 function activate(context) {
+	if (!sessionStartTimestamp) sessionStartTimestamp = Date.now();
 	const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 1000);
 	statusBarItem.command = 'traecord.toggle';
 	statusBarItem.text = '$(plug) Traecord';
@@ -28,7 +30,7 @@ function activate(context) {
 				isConnected = true;
 				updateStatusBar();
 				updatePresence();
-				console.log('RPC Ready.');
+				console.log('[Traecord] RPC Ready.');
 			} else if (message.type === 'error') {
 				console.error('Failed to connect to Discord:', message.error);
 				vscode.window.showErrorMessage(`Failed to connect to Discord: ${message.error}`);
@@ -52,7 +54,7 @@ function activate(context) {
 		isConnected = false;
 		updateStatusBar();
 
-		console.log('Disconnected from Discord.');
+		console.log('[Traecord] Disconnected from Discord.');
 	}
 
 	function updateStatusBar() {
@@ -78,7 +80,7 @@ function activate(context) {
 				type: 'setActivity',
 				data: {
 					details: 'Idling',
-					startTimestamp: Date.now(),
+					startTimestamp: sessionStartTimestamp,
 					largeImageKey: 'idle',
 					largeImageText: 'Idling',
 					smallImageKey: 'trae',
@@ -103,7 +105,7 @@ function activate(context) {
 		const payload = {
 			details: details,
 			state: state,
-			startTimestamp: Date.now(),
+			startTimestamp: sessionStartTimestamp,
 			largeImageKey: language,
 			largeImageText: languageText,
 			smallImageKey: 'trae',
@@ -145,132 +147,3 @@ module.exports = {
 	activate,
 	deactivate
 };
-
-// let client;
-// let startTimestamp = new Date();
-
-// let isConnected = false;
-
-// function activate(context) {
-// 	console.log('Activating Traecord extension.');
-
-// 	const clientId = '817458228731576390';
-// 	const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 1000);
-// 	statusBarItem.command = 'traecord.toggle';
-// 	statusBarItem.text = '$(plug) Traecord';
-// 	statusBarItem.show();
-
-// 	function connect() {
-// 		if (client) return;
-
-// 		client = new rpc.Client({ transport: 'ipc' });
-
-// 		client.on('ready', () => {
-// 			console.log('Connected to Discord.');
-// 			isConnected = true;
-// 			updatePresence();
-// 			updateStatusBar();
-// 		});
-	
-// 		client.login({ clientId }).catch(err => {
-// 			console.error(err);
-// 		});
-// 	}
-
-// 	function disconnect() {
-// 		if (!client) return;
-
-// 		client.clearActivity();
-// 		client.destroy();
-// 		client = null;
-
-// 		isConnected = false;
-// 		console.log('Disconnected from Discord.');
-// 		updateStatusBar();
-// 	}
-
-// 	function updateStatusBar() {
-// 		if (isConnected) {
-// 			statusBarItem.text = '$(check) Traecord';
-// 			statusBarItem.tooltip = 'Connected to Discord';
-// 		} else {
-// 			statusBarItem.text = '$(plug) Traecord';
-// 			statusBarItem.tooltip = 'Click to connect to Discord';
-// 		}
-// 	}
-
-// 	connect();
-
-// 	vscode.window.onDidChangeActiveTextEditor(() => {
-// 		if (isConnected) updatePresence();
-// 	});
-// 	vscode.workspace.onDidChangeTextDocument(() => {
-// 		if (isConnected) updatePresence();
-// 	});
-
-// 	context.subscriptions.push(
-// 		vscode.commands.registerCommand('traecord.toggle', () => {
-// 			if (isConnected) {
-// 				disconnect();
-// 			} else {
-// 				connect();
-// 			}
-// 			updateStatusBar();
-// 		})
-// 	);
-
-// 	// UPDATING PRESENCE
-// 	function updatePresence() {
-// 		const config = vscode.workspace.getConfiguration('traecord');
-// 		const showFileName = config.get('showFileName');
-// 		const showWorkspace = config.get('showWorkspace');
-
-// 		const editor = vscode.window.activeTextEditor;
-
-// 		if (!editor) {
-// 			statusBarItem.text = '$(eye-closed) Traecord';
-
-// 			client.setActivity({
-// 				details: 'Idling',
-// 				startTimestamp,
-// 				largeImageKey: 'idle',
-// 				largeImageText: 'Idling',
-// 				smallImageKey: 'trae',
-// 				smallImageText: 'Trae',
-// 			})
-// 			return;
-// 		};
-
-// 		const fileName = path.basename(editor.document.fileName);
-// 		const workspace = vscode.workspace.name || 'No workspace';
-// 		const language = editor.document.languageId;
-// 		const languageText = `Editing a ${language} file`;
-
-// 		const details = showFileName ? `Editing ${fileName}` : 'Idling';
-// 		const state = showWorkspace ? `In ${workspace}` : 'No workspace';
-
-// 		client.setActivity({
-// 			details: details,
-// 			state: state,
-// 			startTimestamp,
-// 			largeImageKey: language,
-// 			largeImageText: languageText,
-// 			smallImageKey: 'trae',
-// 			smallImageText: 'Trae',
-// 		});
-
-// 		console.log(`Updated presence: ${language}`);
-// 	}
-// }
-
-// function deactivate() {
-// 	if (client) {
-// 		client.clearActivity();
-// 		client.destroy();
-// 	}
-// }
-
-// module.exports = {
-// 	activate,
-// 	deactivate
-// };
